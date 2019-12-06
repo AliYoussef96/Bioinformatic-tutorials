e700e57ae942dc114720cb1408590d8357a53512
Ali Youssef <aliali.mostafa99@gmail.com>
2019-12-06 16:08:10 +0200

    Add files via upload

---
+++ R.code/4.PatternMatchingProblem.r
@@ -0,0 +1,17 @@
+# Pattern Matching Problem
+
+
+
+library("seqinr")
+genome <- read.fasta(file = "/home/ali/Nermen_bioinfo_courses/youtube/data/vibrio_cholerae.fasta",
+                     as.string = TRUE)
+
+genome1 <- as.character(genome$NC_002505.1)
+genome2 <- as.character(genome$NC_002506.1)
+
+library("Biostrings")
+genome1 <- DNAString(genome1)
+genome1 <- DNAString(genome2)
+
+patterns <- matchPattern('ATGATCAAG', genome1)
+patterns@ranges@start
