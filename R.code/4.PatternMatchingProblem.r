8766c140260538082c1733f2a1e7a37ac83a8b92
Ali Youssef <aliali.mostafa99@gmail.com>
2019-12-06 16:23:50 +0200

    Update 4.PatternMatchingProblem.r

--- R.code/4.PatternMatchingProblem.r
+++ R.code/4.PatternMatchingProblem.r
@@ -1,7 +1,7 @@
 # Pattern Matching Problem
 
 library("seqinr")
-genome <- read.fasta(file = "vibrio_cholerae.fasta",
+genome <- read.fasta(file = "/home/ali/Nermen_bioinfo_courses/youtube/data/vibrio_cholerae.fasta",
                      as.string = TRUE)
 
 genome1 <- as.character(genome$NC_002505.1)
@@ -9,7 +9,10 @@ genome2 <- as.character(genome$NC_002506.1)
 
 library("Biostrings")
 genome1 <- DNAString(genome1)
-genome1 <- DNAString(genome2)
+genome2 <- DNAString(genome2)
 
-patterns <- matchPattern('ATGATCAAG', genome1)
-patterns@ranges@start
+pattern1 <- matchPattern('ATGATCAAG', genome1)
+pattern1@ranges@start
+
+pattern2 <- matchPattern('ATGATCAAG', genome2)
+pattern2@ranges@start
